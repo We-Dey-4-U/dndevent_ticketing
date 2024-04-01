5197e34898c692770d862d8d35b0ceed90e4361b
ikenna <mrteks2022@gmail.com>
2024-04-01 11:32:22 -0700

    re

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -8,7 +8,7 @@ router.get('/tickets/user/:userId', ticketController.getUserTickets);
 //router.get('/tickets/:userId', ticketController.getUserTickets);
 //router.get('/tickets/user', ticketController.getCurrentUserTickets); // New route for fetching current user's tickets
 // Prefix the routes with the new server URL
-const SERVER_URL = 'https://ticket-backend-1-09ex.onrender.com/';
+const SERVER_URL = 'https://ticket-backend-1-09ex.onrender.com';
 
 router.post(`${SERVER_URL}/api/tickets`, ticketController.purchaseEventTicket);
 router.get(`${SERVER_URL}/api/tickets/user/:userId`, ticketController.getUserTickets);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.post('/register', userController.registerUser);
-router.post('/login', userController.loginUser);
-router.put('/users/:email', userController.updateUserToAdmin); // New route for updating user to admin
+//router.post('/register', userController.registerUser);
+//router.post('/login', userController.loginUser);
+//router.put('/users/:email', userController.updateUserToAdmin); // New route for updating user to admin
 
 
 // Prefix the routes with the new server URL
-const SERVER_URL = 'https://ticket-backend-1-09ex.onrender.com/';
+const SERVER_URL = 'https://ticket-backend-1-09ex.onrender.com';
 
 router.post(`${SERVER_URL}/api/register`, userController.registerUser);
 router.post(`${SERVER_URL}/api/login`, userController.loginUser);
